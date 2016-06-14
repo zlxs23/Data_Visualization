f837584ade21d9eeedea49483529d2435867999d
zlxs23 <zilanxingshuo@gmail.com>
2016-06-14 14:16:46 +0800

    update Chap 3.7

--- Code_Backup.py
+++ Code_Backup.py
@@ -647,14 +647,198 @@ import pylab as p
 import scipy.signal as signal
 
 # get some linear data
-x = np.linspace(0,1,101)
+x = np.linspace(0, 1, 101)
 
 # add some noisy signal
 x[3::10] = 1.5
 
 p.plot(x)
-p.plot(signal.medfilt(x,3))
-p.plot(signal.medfilt(x,5))
+p.plot(signal.medfilt(x, 3))
+p.plot(signal.medfilt(x, 5))
 
-p.legend(['original signal','length 3','length 5'])
+p.legend(['original signal', 'length 3', 'length 5'])
 p.show()
+
+'''
+3.2.2
+定义图表类型 -- 柱状图，线形图 AND 堆积柱状图
+'''
+plot([1, 2, 3, 3, 2, 3, 3, 2, 2, 1])
+plot([4, 3, 2, 1], [1, 2, 3, 4])
+
+from matplotlib.pyplot import *
+
+# some simple data
+x = [1, 2, 3, 4]
+y = [5, 4, 3, 2]
+
+# create new figure
+figure()
+
+# divide subplots into 2 to 3 grid
+# select #1 线性图
+subplot(231)
+plot(x, y)
+
+# select #2 竖向柱状图（默认）
+subplot(232)
+bar(x, y)
+
+# select #3 横向柱状图
+# horizontal bar-charts
+subplot(233)
+barh(x, y)
+
+# select #4 堆叠柱状图
+# stacked bar charts
+subplot(234)
+bar(x, y)
+# more data for stacked bar charts
+y1 = [7, 8, 5, 3]
+bar(x, y1, bottom=y, color='r')    # 设置 argument bottom = y [底部为 y 则 y1 在上部]
+
+# select #5 箱线图
+# box plot
+subplot(235)
+boxplot(x)
+
+# select #6 散点图
+# scatter plot
+subplot(236)
+scatter(x, y)
+
+'''
+用统一数据来绘制比较箱线图 AND 直方图
+'''
+
+from pylab import *
+
+dataset = [113, 115, 119, 121, 124,
+           124, 125, 126, 126, 126,
+           127, 127, 128, 129, 130,
+           130, 131, 132, 133, 136]
+subplot(121)
+boxplot(dataset, vert=False)
+subplot(122)
+hist(dataset)
+
+'''
+3.3
+简单的正弦图 AND 余弦图
+From -pi to pi 具有相同的线性距离 の 256 个点来计算正弦值 AND 余弦值
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+yc = np.cos(x)
+ys = np.sin(x)
+
+plt.plot(x, yc)
+plt.plot(x, ys)
+
+plt.show()
+'''
+以上述简单图表为基础 进一步定制化添加更多信息
+同时让坐标轴 AND 边界更加精确
+'''
+from pylab import *
+
+# generator uniformly distribute
+# 256 points from -pi to pi,inclusive
+x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+
+# these are bectorised versions of math.cos and math.sin in built-in python maths
+# compute cos for every x
+yc = np.cos(x)
+# compute sin for every x
+ys = np.sin(x)
+
+# plot cos
+plot(x, yc)
+# plot sin
+plot(x, ys)
+
+# define plot title
+title('Functions $\sin$ and $\cos$')
+# set x,y limit
+xlim(-3.0, 3.9)
+ylim(-1.0, 1.0)
+
+# format ticks at specific values
+xticks([-np.pi, -np.pi/2, 0, np.pi/2, np.pi],
+       [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$\pi/2$', r'$\pi$'])
+yticks([-1, 0, +1],
+       [r'$-1$', r'$0$', r'$1$'])
+show()
+
+'''
+改变线 の 属性
+'''
+# way 1
+plot(x, y, linewidth=1.5)
+# way 2
+line = plot(x, y)
+line.set_linewidth(1.5)
+# way 3
+line = plot(x, y)
+setp(line, 'linewidth', 1.5)    # OR
+setp(line, linewidth=1.5)
+
+'''
+3.6.2
+使用 matplotlib.pyplot.locator_params() 方法控制刻度容器定位器 の 行为
+刻度位置通常会自动确定下来 我们还是可控刻度的数目 以及在 plot 比较小时使用一个 紧凑 试图 tight view
+'''
+from pylab import *
+# get current axis
+ax = gca()
+# set view to tight and maxium number of tick intervals to 10
+ax.locator_params(tight=True, nbins=10)
+# generator 100 normal distribution values
+ax.plot(np.random.normal(10, .1, 100))
+show()
+
+'''
+使用 matplotlib.ticker.FormatStrFormatter 刻度格式器 规定 刻度 值(通常是 数字)的显示方式
+Eg: 可方便指定   '%2.1f' OR '%1.1f cm' 格式字符串 作为刻度标签
+这里使用 dates module 来 实现时间刻度
+'''
+from pylab import *
+import matplotlib as mpl
+import datetime
+
+fig = figure()
+
+# get current axis
+ax = gca()
+
+# set some daterange
+start = datetime.datetime(2016, 1, 1)
+stop = datetime.datetime(2016, 12, 31)
+delta = datetime.timedelta(days=1)
+
+# convert dates for matplotlib
+dates = mpl.dates.drange(start, stop, delta)
+
+# generator some random values
+values = np.random.rand(len(dates))
+
+ax = gca()
+
+# create plot with dates
+ax.plot_date(dates, values, linestyle='-', marker='')
+
+# specify formater
+date_format = mpl.dates.DateFormatter('%Y-%m-%d')
+
+# apply formater
+ax.xaxis.set_major_formatter(date_format)
+
+# atuoformat date labels
+# rotates labels by 30 degrees by default
+# use rotate param to specify different rotation degree
+# use bottom param to give more room to date labels
+fig.autofmt_xdate()
+
+show()
